22217fa6f52b66e12a3062676aa605e40363684f
Agnieszka <agnieszka.koszany@codecool.com>
2017-05-19 02:14:10 +0200

    sorting_algoritms module added

---
+++ sorting_algorithms.py
@@ -0,0 +1,35 @@
+def read_data(amount):
+    pass
+
+
+def save_data(numbers):
+    pass
+
+
+def bubble_sort(numbers):
+    pass
+
+
+def insertion_sort(numbers):
+    pass
+
+
+def sort_data(sort_type='bubble', amount):
+    pass
+
+
+def get_computing_time(computing_type, amount):
+    """ Params:
+        computing type: 'import data'
+                        'export data'
+                        'bubble sort'
+                        'insertion sort'
+
+        Returns:
+        computing time in ms
+    """
+    pass
+
+
+def print_sort_summary():
+    pass
